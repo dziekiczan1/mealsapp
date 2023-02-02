a69183a981ff6aecb6bae7fe63933762498141b3
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-02-02 18:29:35 +0100

    add useLayoutEffect do display title dynamically

--- App.js
+++ App.js
@@ -27,7 +27,14 @@ export default function App() {
               title: "All Categories",
             }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId;
+            //   return { title: catId };
+            // }} // we get route and navigation properties automatically by react navigation passed to into this function when we define such function for options
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
