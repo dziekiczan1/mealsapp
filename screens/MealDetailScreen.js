e329141b029e1b26cb21cff01d167985c84c19b3
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-02-04 16:36:20 +0100

    add Subtitle and List components

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,5 +1,7 @@
-import { Text, Image, View } from "react-native";
+import { Text, Image, View, StyleSheet } from "react-native";
 
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/data";
 
@@ -8,17 +10,37 @@ function MealDetailScreen({ route }) {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} />
-      <Text>{selectedMeal.title}</Text>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         affordability={selectedMeal.affordability}
         complexity={selectedMeal.complexity}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
-      <Text>Steps</Text>
+      <Subtitle>Ingredients</Subtitle>
+      <List data={selectedMeal.ingredients} />
+      <Subtitle>Steps</Subtitle>
+      <List data={selectedMeal.steps} />
     </View>
   );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+});
